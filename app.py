0a54352b574cf0f4405f735ff666f85aa4798122
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-08-08 21:08:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,7 @@ def DataToGoogleSheet(gc,dt2,message,datatype):
         datasheet,Month = MoneyGoogleSheet(dt2,gc)
     elif datatype == 'Motor':
         datasheet=MotorGoogleSheet(dt2,gc)      
-    values =  [[dt2.strftime("%d")]+message]
-    values = [item for sublist in values for item in sublist]
+    values =  [dt2.strftime("%d")]+message
     print(values,type(values))
     datasheet.append_table(values=values)#這一行資料輸入完整 但是會失敗0727 
 #===============================================================================
