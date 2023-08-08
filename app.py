8db2d5ce490a96f32e6751fdc666b4816f8de8c9
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-08-08 18:09:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,7 @@ def handle_message(event):
     reply_arr=[]
     global previous_message
     print("input : ",event.message.text,type(event.message.text))
+    logging.debug('debug message')
     #Google試算表教學網頁 https://www.wongwonggoods.com/all-posts/python/python_web_crawler/python-pygsheets/
     auth_file = "linebotsheet.json"
     gc = pygsheets.authorize(service_file = auth_file)
