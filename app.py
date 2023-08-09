846e56c985b3237c53fc97968e3d3e1ba72f2757
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-08-09 20:33:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def two_dimensional_list_intto_str(range_of_cells):
     result_str = result_str.strip()
     return result_str
 #===============================================================================
-def day_lessmoney(dt2,gc,Month,reply_arr):
+def day_lessmoney(dt2,gc,reply_arr):
     datasheet,Month = MoneyGoogleSheet(dt2,gc)
     RemainingCost = datasheet.cell('D2')
     Remaining=RemainingCost.value
@@ -109,7 +109,7 @@ def day_lessmoney(dt2,gc,Month,reply_arr):
     # 计算当前月份的总天数
     total_days_in_month = (next_month - first_day_of_month).days
     expenses_remaining=int(Remaining)/(int(total_days_in_month)-int(day))
-    return expenses_remaining
+    return expenses_remaining,reply_arr
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -138,7 +138,7 @@ def handle_message(event):
             reply_arr=OriginalReply.textReply(reply_arr,"GoogleSheet上傳測試失敗")        
     
     elif(event.message.text == '當月剩餘費用'):
-        expenses_remaining=day_lessmoney(dt2,gc,Month,reply_arr)
+        expenses_remaining,reply_arr=day_lessmoney(dt2,gc,reply_arr)
         reply_arr=OriginalReply.textReply(reply_arr,"平均每日伙食費剩下 : "+str("{:.2f}".format(expenses_remaining))+"元")
         if expenses_remaining<=200:
             reply_arr=OriginalReply.textReply(reply_arr,"花太多錢啦!省錢一點")
@@ -212,7 +212,7 @@ def handle_message(event):
             
             DataToGoogleSheet(gc,dt2,data_list,'Money')
             
-            expenses_remaining=day_lessmoney(dt2,gc,Month,reply_arr)
+            expenses_remaining,reply_arr=day_lessmoney(dt2,gc,reply_arr)
             reply_arr=OriginalReply.textReply(reply_arr,"平均每日伙食費剩下 : "+str("{:.2f}".format(expenses_remaining-data_list[3]))+"元")
             reply_arr=OriginalReply.textReply(reply_arr,"記帳成功")
         except:      
