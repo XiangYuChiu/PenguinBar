9ba92c53b0fd0e833acb993dc2966db0d46f8a2c
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-08-10 17:22:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ def handle_message(event):
             DataToGoogleSheet(gc,dt2,data_list,'Money')
             expenses_remaining,RemainingCost=month_lessmoney(dt2,gc)
             datasheet,Month = MoneyGoogleSheet(dt2,gc)
-            TodayMoney = datasheet.cell('O1')
+            LineBank = datasheet.cell('I2')
             #TodayMoney = datasheet.cell('O'+str(int(dt2.strftime("%d"))))
             reply_arr=OriginalReply.textReply(reply_arr,"今日預算剩下:"+str(("{:.2f}".format(RemainingCost-TodayMoney))))
             reply_arr=OriginalReply.textReply(reply_arr,"記帳成功")
