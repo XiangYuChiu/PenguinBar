5de67d3d180fd3870ad867c9f97839324401c85d
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-08-18 17:01:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -238,9 +238,8 @@ def handle_message(event):
             #reply_arr=OriginalReply.textReply(reply_arr,str(day))
             TodayMoney = datasheet.cell('O'+str(int(day)+24)).value
             expenses_remaining,RemainingCost=month_lessmoney(dt2,gc)
-            print("1",expenses_remaining)
-            print("2",TodayMoney)
-            #reply_arr=OriginalReply.textReply(reply_arr,"本日預算 : "+str("{:.2f}".format(expenses_remaining))+"元\n今天伙食費剩下 : "+str("{:.2f}".format((expenses_remaining)-int(TodayMoney)))+"元\n今天總花費"+str(TodayMoney)+"元")
+
+            reply_arr=OriginalReply.textReply(reply_arr,"本日預算 : "+str("{:.2f}".format(expenses_remaining))+"元\n今天伙食費剩下 : "+str("{:.2f}".format((expenses_remaining)-int(TodayMoney)))+"元\n今天總花費"+str(TodayMoney)+"元")
             reply_arr=OriginalReply.textReply(reply_arr,"記帳成功")
         except Exception as e:      
             reply_arr=OriginalReply.textReply(reply_arr,"小企鵝壞掉了Q_Q \n原因 : "+str(e))
