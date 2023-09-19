0fdbf8f24e3e5fc0e85fc20a08b071bf8a5fa945
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-09-19 22:20:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,67 +32,8 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 #===============================================================================
-'''
-# 處理GoogleSheet
-# 傳遞到GoogleSheet所使用的函示庫
-import pygsheets
-def MoneyGoogleSheet(dt2,gc,move=0):
-    #print('MoneyGoogleSheet')
-    sheet_url = "https://docs.google.com/spreadsheets/d/1jnKkUIegnTrr1nA-fCCp9i-sOoiB3_of1Ry5uwUFSvI/edit#gid=1747979925/"
-    sheet = gc.open_by_url(sheet_url)
-    try:
-        Month = dt2.strftime("%m")
-        print(str(int(Month))+"月預算")
-        datasheet = sheet.worksheet_by_title(str(int(Month)+int(move))+"月預算")
-    except:
-        print("沒有獲取到資料表")
-        datasheet = sheet[1]
-    return datasheet,Month
-def TestGoogleSheet(dt2,gc):  
-    sheet_url = "https://docs.google.com/v/d/1nwrWBRMlfm-9oAnIeSXfuw5qKxqa4B4HCxCIUf36faM/edit#gid=0/"
-    sheet = gc.open_by_url(sheet_url)
-    datasheet = sheet[0]
-    return datasheet
 
-def MotorGoogleSheet(dt2,gc): 
-    sheet_url = "https://docs.google.com/spreadsheets/d/1CKLgM4DdJ4njJEhMADszGo6skja0As520Lmgb6iwgcc/edit#gid=0"
-    sheet = gc.open_by_url(sheet_url)
-    datasheet = sheet.worksheet_by_title("MYN-7371") 
-    return datasheet
-    
-def DataToGoogleSheet(gc,dt2,message,datatype): 
-    #print("DataToGoogleSheet")
-    if datatype == 'Test':
-        datasheet=TestGoogleSheet(dt2,gc)
-    elif datatype == 'Money':
-        datasheet,Month = MoneyGoogleSheet(dt2,gc)
-    elif datatype == 'Motor':
-        datasheet=MotorGoogleSheet(dt2,gc)      
-    values =  [dt2.strftime("%d")]+message
-    print(values,type(values))
-    try:
-        b_column_values = datasheet.get_col(2, returnas='matrix', include_tailing_empty=False)
-        last_row_index = len(b_column_values)
-        datasheet.append_table(start='B' + str(last_row_index + 1), end='B' + str(last_row_index + 1), values=values)
-        #datasheet.append_table(values)#這一行資料輸入完整 但是會失敗0727
-    except Exception as e:
-        print("寫入GoogleSheet error: ",e)
-'''
-#===============================================================================
-#工具類
-def two_dimensional_list_intto_str(range_of_cells):
-    # 使用循环将嵌套列表转换为字符串
-    result_str = ''
-    for sublist in range_of_cells:
-        for inner_list in sublist:
-            for item in inner_list:
-                result_str += item + ' '
-        
-    # 去除字符串末尾多余的空格
-    result_str = result_str.strip()
-    return result_str
 #===============================================================================
 def month_lessmoney(dt2,gc):
     datasheet,Month = tool.MoneyGoogleSheet(dt2,gc)
@@ -214,23 +155,23 @@ def handle_message(event):
         elif(event.message.text == '記帳類別'):
             datasheet,Month = tool.MoneyGoogleSheet(dt2,gc)
             range_of_cells = datasheet.get_values_batch( ['K3:K11'])
-            result_str = two_dimensional_list_intto_str(range_of_cells)
+            result_str = tool.two_dimensional_list_intto_str(range_of_cells)
             reply_arr=OriginalReply.textReply(reply_arr,result_str)
         elif(event.message.text == '記帳帳號'):
             datasheet,Month = tool.MoneyGoogleSheet(dt2,gc)
             range_of_cells = datasheet.get_values_batch( ['H2:H7'])
-            result_str = two_dimensional_list_intto_str(range_of_cells)
+            result_str = tool.two_dimensional_list_intto_str(range_of_cells)
             reply_arr=OriginalReply.textReply(reply_arr,result_str)
         elif(event.message.text == '記帳格式'):
             datasheet,Month = tool.MoneyGoogleSheet(dt2,gc)
             range_of_cells = datasheet.get_values_batch( ['C14:F14'])
-            result_str = two_dimensional_list_intto_str(range_of_cells)
+            result_str = tool.two_dimensional_list_intto_str(range_of_cells)
             reply_arr=OriginalReply.textReply(reply_arr,result_str)
             
         elif(event.message.text == '汽機車格式'):
             datasheet = tool.MotorGoogleSheet(dt2,gc)
             range_of_cells = datasheet.get_values_batch( ['B6:E6'])
-            result_str = two_dimensional_list_intto_str(range_of_cells)
+            result_str = tool.two_dimensional_list_intto_str(range_of_cells)
             reply_arr=OriginalReply.textReply(reply_arr,result_str)
         else:         
             if previous_message:
