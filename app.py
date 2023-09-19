803b772cbb69eaa2a0047c71cd6b0a97126dba30
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-09-19 23:09:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def handle_message(event):
             reply_arr=OriginalReply.textReply(reply_arr,"記帳成功")
         elif(previous_message == '記帳-收入'): 
             previous_message = ""
-            reply_arr.append(tool.create_dropdown_menu())
+            reply_arr.append(tool.create_dropdown_menu(previous_message))
         elif(event.message.text == '記帳-支出'):
             reply_arr=OriginalReply.textReply(reply_arr,'進入記帳-支出模式')
             previous_message='記帳-支出'
