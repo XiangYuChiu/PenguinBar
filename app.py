52650de7f1691f7b16832b5461b32099b273c4bf
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-09-19 23:24:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def handle_message(event):
             reply_arr=OriginalReply.textReply(reply_arr,result_str)
             
         elif(event.message.text == 'test'):    
-            reply_arr.append(tool.create_default_dropdown_menu())
+            reply_arr=(tool.create_default_dropdown_menu())
             
         else:         
             if previous_message:
