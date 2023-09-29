4d93b77b882cea0b53a3ad3c27991a59c5d2f8bb
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-09-29 17:31:31 +0800

    Update MoneyReply.py

--- MoneyReply.py
+++ MoneyReply.py
@@ -4,7 +4,7 @@ import json
 import OriginalReply,tool
 
 
-def lastest_four_data(x=4,dt2,gc):
+def lastest_four_data(dt2,gc,x=4):
     worksheet,Month = tool.MoneyGoogleSheet(dt2,gc)    
     # 找到C列(3)到F列(6)非空白的数据的最后4笔新增数据
     non_empty_data=[]
