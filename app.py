ff9bc377e2057a6c53fd289f5b8c21948946cae1
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-10-04 17:19:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -176,8 +176,32 @@ def handle_message(event):
             
         elif(event.message.text == 'test'):  
             print("Enter test")
-            reply_arr.append(Reply.DefaultQuickReply())
+            msg = event.message.text
+            img_search = {'tbm': 'isch', 'q': msg}
+            query = urllib.parse.urlencode(img_search)
+            base  = "https://www.google.com/search?"
+            url   = str(base+query)
             
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+            
+            res  = urllib.request.Request(url, headers=headers)
+            con  = urllib.request.urlopen(res)
+            data = con.read()
+    
+            pattern = '"(https://encrypted-tbn0.gstatic.com[\S]*)"'
+            img_list = []
+    
+            for match in re.finditer(pattern, str(data, "utf-8")):
+                if len(match.group(1)) < 150:
+                    img_list.append(match.group(1))
+    
+            random_img_url = img_list[random.randint(0, len(img_list)+1)]
+    
+            message = ImageSendMessage(
+                original_content_url = random_img_url,
+                preview_image_url    = random_img_url
+                )
+            reply_arr=Reply.ImageReply(reply_arr,message)
         else:         
             if previous_message:
                 reply_arr=Reply.textReply(reply_arr,previous_message)
