2bc8b82a171e4851622550fca41edc5327bdce3e
XiangYuChiu <61840928+XiangYuChiu@users.noreply.github.com>
2023-10-31 21:57:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def handle_message(event):
             print(len(account),balance,account)
             finial = []
             for i in range(len(account)):
-                finial.append(account[i]+" : "+balance[i]+"$\n")
+                finial.append(account[i]+" : "+balance[i]+"$")
             print(finial)
             reply_arr.append(Reply.creat_CarouselColumn(finial))
         elif(event.message.text == '當月剩餘費用'):
